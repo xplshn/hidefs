cdcb7cd9f9bf29c032d8f9a71487cfeccf95a45e
xplshn <114888778+xplshn@users.noreply.github.com>
2024-12-15 08:23:08 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,5 @@
 #!/bin/sh -e
 
-# Linus and Linux, two things that suck, they shove their GPL nonsense down devs'
-# throats. I only write public domain or equivalent code, and it'll stay that way
-# forever.
-
-### Fake the license, tell the kernel that the license is GPL so that it stops whining about the law and other imaginary things
 SEED="_$RANDOM_$(date +%s)"
 sed_in_place() {
   cp "$1" "$1.$SEED.bak"
